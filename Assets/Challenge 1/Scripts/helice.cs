12d6c323562488cafd44fa2f02dbb55cdf0fe81e
gabyguwu <fabianagjo@gmail.com>
2026-03-06 12:08:46 +0000

    Avião exercicio
    Gabriela a63568

---
+++ Assets/Challenge 1/Scripts/helice.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class helice : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward *2);
+    }
+}
